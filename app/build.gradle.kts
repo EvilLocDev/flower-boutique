ecb5b513236447695880137a12fa26a45ef21dd1
XuanChinh <huynhxuanchinh2004@gmail.com>
2026-03-21 05:26:54 +0700

    merge giaodien va su ly tao don thanh toan from xuanchinh branch at 19h40 22/03/2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 
 android {
     namespace = "com.example.flowerboutique"
-    compileSdk = 36
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
         applicationId = "com.example.flowerboutique"
@@ -31,11 +33,6 @@ android {
             )
         }
     }
-
-    buildFeatures {
-        viewBinding = true
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
